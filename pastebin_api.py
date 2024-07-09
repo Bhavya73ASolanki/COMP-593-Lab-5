a7b28abece11239326cd454634c7bb8bf035d038
Bhavya73ASolanki <Bhavykumarmanishbhai.Solanki@flemingcollege.ca>
2024-07-08 23:37:25 -0400

    Completed Pastebin_api.py

--- pastebin_api.py
+++ pastebin_api.py
@@ -4,10 +4,34 @@ https://pastebin.com/doc_api
 '''
 import requests
 
-PASTEBIN_API_POST_URL = 'https://pastebin.com/api/api_post.php'
-API_DEV_KEY = 'Put your API key here'
+PASTEBIN_API_POST_URL = 'https://pastebin.com/doc_api '
+API_DEV_KEY = 'K-OqOvRLRxaO3tPj7vlyQSozF9y-AMNs'
 
 def post_new_paste(title, body_text, expiration='N', listed=True):
+    
+    params = {
+        'api_dev_key': API_DEV_KEY,
+        'api_option': 'paste',
+        'api_paste_code': body_text,
+        'api_paste_name': title,
+        'api_paste_expire_date': expiration,
+        'api_paste_private': 0 if listed else 1
+    }
+    
+    
+    print("Sending request to pastebin_api")
+    response = requests.post(PASTEBIN_API_POST_URL, data=params)
+    
+    
+    if response.status_code == 200 and response.text.startswith('https://'):
+        print("Paste created successfully!")
+        return response.text
+    else:
+        print("Failed to paste. Response status code:", response.status_code)
+        print("Response text:", response.text)
+        return None
+    
+    
     """Posts a new paste to PasteBin
 
     Args:
@@ -19,6 +43,10 @@ def post_new_paste(title, body_text, expiration='N', listed=True):
     Returns:
         str: URL of new paste, if successful. Otherwise None.
     """    
-    # TODO: Function body
-    # Note: This function will be written as a group 
-    return
+    if __name__ == "__main__":
+    
+     paste_url = post_new_paste("Test Paste", "This is a test paste.", "10M", True)
+    if paste_url:
+        print("Paste URL:", paste_url)
+    else:
+        print("Failed")
