27db244c36abbcce2aac29d30e16c2e2336b7401
Bhavya73ASolanki <Bhavykumarmanishbhai.Solanki@flemingcollege.ca>
2024-07-08 23:21:39 -0400

    Completed Poke_api.py

--- poke_api.py
+++ poke_api.py
@@ -29,7 +29,18 @@ def get_pokemon_info(pokemon_name):
 
     # TODO: If the GET request failed, print the error reason and return None
 
-    return
+    pokemon = str(pokemon).strip().lower()
+    print(f"Getting information on {pokemon}...")
+
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print("Successfully fetched Pokémon Info")
+        return response.json()
+    else:
+        print(f"Failed to fetch Pokémon. Status code: {response.status_code}")
+        return None
 
 if __name__ == '__main__':
     main()
